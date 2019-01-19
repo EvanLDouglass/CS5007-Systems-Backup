e6f5bdf179eda4933bc5c1a1fdad8cc8953c68b6
evandouglass <douglass.ev@husky.neu.edu>
2019-01-19 00:37:55 +0000

    Add a2 and initial files

---
+++ a2/fastsort.c
@@ -0,0 +1,18 @@
+/*
+ * CS5007: Assignment 2
+ * Fastsort: This file implements Quick Sort and tests it using variable length inputs
+ * Author: Evan Douglass
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void quickSort(int arr[]) {
+
+}
+
+int main() {
+    
+    return 0;
+}
---
+++ a2/slowsort.c
@@ -0,0 +1,18 @@
+/*
+ * CS5007: Assignment 2
+ * Slowsort: This file implements Insertion Sort and tests it with differnt length input arrays
+ * Author: Evan Douglass
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void insertionSort(int arr[]) {
+
+}
+
+int main() {
+
+    return 0;
+}
