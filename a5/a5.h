5cd381c46f8ac9c9a42cf4ff7abc22ee978d0cf1
evandouglass <douglass.ev@husky.neu.edu>
2019-02-18 23:15:14 +0000

    Start for bit manipulation

--- a5/a5.h
+++ a5/a5.h
@@ -10,3 +10,14 @@
  * ==========================
  */
 
+// Packs two characters into a single 2 byte unsigned variable.
+unsigned int packCharacters(char c1, char c2);
+
+// Unpacks and prints two characters from a 2 byte unsigned variable that has been
+// previously packed.
+void unpackCharacters(unsigned int packed);
+
+// Uses bit manipulation to calculate a number to a given power of two and
+// prints the results.
+void power2(unsigned int number, int pow);
+
