e92ae0b35613e35a8bb06f73a23f0fac59ceeada
evandouglass <douglass.ev@husky.neu.edu>
2019-02-18 22:14:51 +0000

    Create start files

---
+++ a5/a5.h
@@ -0,0 +1,12 @@
+/* 
+ * CS5007
+ * A5: Header File
+ * Author: Evan Douglass
+ * Created: Feb. 17th 2019
+ */
+
+/*
+ * Bit Manipulation Functions
+ * ==========================
+ */
+
---
+++ a5/a5_bits.c
@@ -0,0 +1,9 @@
+/* 
+ * CS5007
+ * A5: Bit Manipulation
+ * Author: Evan Douglass
+ * Created: Feb. 17th 2019
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
