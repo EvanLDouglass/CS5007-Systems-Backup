1b408eb370937394bbaf00c15275418e13025706
evandouglass <douglass.ev@husky.neu.edu>
2019-02-22 13:02:28 -0800

    Create a5_structs.c

---
+++ a5/a5_structs.c
@@ -0,0 +1,14 @@
+/*
+ * CS5007
+ * A5: Dijkstra Data Structures
+ * Author: Evan Douglass
+ * Created: Feb. 20 2019
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#include "a5.h"
