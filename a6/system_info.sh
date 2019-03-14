f12fab1aab5914ab69ce0616d8f27adf63d3ebc4
evandouglass <douglass.ev@husky.neu.edu>
2019-03-14 01:32:56 +0000

    Part 2 done

--- a6/system_info.sh
+++ a6/system_info.sh
@@ -12,8 +12,14 @@ echo "cores: $PROC"
 # Regex syntax for bash found at
 # https://www.geeksforgeeks.org/awk-command-unixlinux-examples/
 # grep portion finds a line with the memory type and pipes
-# it into awk, which prints the 2nd and 3rd fields in the
-# line, the number and units
-TOTAL=$(grep MemTotal /proc/meminfo | awk '{print $2,$3}')
-FREE=$(grep MemFree /proc/meminfo | awk '{print $2,$3}')
-echo "memory: $FREE/$TOTAL"
+# it into awk, which converts the number from kilobytes into
+# megabytes and prints the result.
+TOTAL=$(grep MemTotal /proc/meminfo | awk '{print $2/1000}')
+FREE=$(grep MemFree /proc/meminfo | awk '{print $2/1000}')
+echo "memory: $FREE MB/$TOTAL MB"
+
+# Display partition storage
+# The awk command adds 0 to the field to convert it to a number
+SIZE=$(df -H / | grep /dev/sda2 | awk '{print $2+0}')
+AVAIL=$(df -H / | grep /dev/sda2 | awk '{print $4+0}')
+echo "primary partition storage: $AVAIL GB/$SIZE GB"
