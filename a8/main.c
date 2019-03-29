e6fd91616d00f5dc1b51a7c1e881d74461c4245b
evandouglass <douglass.ev@husky.neu.edu>
2019-03-28 19:32:57 -0700

    Update main.c

--- a8/main.c
+++ a8/main.c
@@ -26,7 +26,7 @@ Index docIndex;
  * Open the specified file, read the specified row into the
  * provided pointer to the movie.
  */
-int CreateMovieFromFileRow(char *file, long rowId, Movie* movie) {
+int CreateMovieFromFileRow(char *file, long rowId, Movie** movie) {
   // TODO: Read a specified row from a specified file into Movie.
   // Need to:
   //   open file
@@ -41,7 +41,7 @@ int CreateMovieFromFileRow(char *file, long rowId, Movie* movie) {
   while ((row = fgets(row, 1000, f)) != NULL) {
     count++;
     if (count == rowId) {
-      movie = CreateMovieFromRow(row);
+      *movie = CreateMovieFromRow(row);
       fclose(f);
       return 0;
     }
@@ -65,7 +65,7 @@ void doPrep(char *dir) {
   // Index the files
   printf("Parsing and indexing files...\n");
   ParseTheFiles(docs, docIndex);
-  printf("%d entries in the index.\n", NumElemsInHashtable(docIndex));
+  printf("%d entries in the index.\n", NumElemsInHashtable(docIndex->ht));
 }
 
 void runQuery(char *term) {
@@ -73,6 +73,7 @@ void runQuery(char *term) {
   // Figure out how to get a set of Movies and create
   // a nice report from them.
   SearchResultIter results = FindMovies(docIndex, term);
+  LinkedList movies = CreateLinkedList();
   if (results == NULL) {
     printf("No results for this term. Please try another.\n");
     return;
@@ -82,9 +83,14 @@ void runQuery(char *term) {
       printf("Couldn't malloc SearchResult in main.c\n");
       return;
     }
-
     int result;
     char *filename;
+    
+    // Get the first
+    SearchResultGet(results, sr);
+    filename = GetFileFromId(docs, sr->doc_id);
+    // TODO: What to do with the filename?
+    
     Movie* mov = NULL;
     SetOfMovies set = CreateSetOfMovies("Search Results");
     // Check if there are more
@@ -100,17 +106,13 @@ void runQuery(char *term) {
       CreateMovieFromFileRow(filename, sr->row_id, mov);
       AddMovieToSetOfMovies(set, mov);
     }
-    // Get the last
-    SearchResultGet(results, sr);
-    filename = GetFileFromId(docs, sr->doc_id);
 
-    // TODO: What to do with the filename?
     CreateMovieFromFileRow(filename, sr->row_id, mov);
     AddMovieToSetOfMovies(set, mov);
 
     free(sr);
     DestroySearchResultIter(results);
-    // Now that you have all the search results, print them out nicely.
+    // TODO: Now that you have all the search results, print them out nicely.
     OutputMovieSet(set);
   }
 }
@@ -143,7 +145,7 @@ int main(int argc, char *argv[]) {
   doPrep(argv[1]);
   runQueries();
 
-  DestroyIndex(docIndex);
+  DestroyOffsetIndex(docIndex);
 
   DestroyDocIdMap(docs);
 }
